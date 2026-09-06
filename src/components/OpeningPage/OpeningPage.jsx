b4bc5875745fde2e90480b1ef3548caba6ef5c40
madortill <mador.till@gmail.com>
2026-09-06 19:51:03 +0300

    with end page!

--- src/components/OpeningPage/OpeningPage.jsx
+++ src/components/OpeningPage/OpeningPage.jsx
@@ -1,93 +1,108 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./EndPage.module.css";
+import styles from "./OpeningPage.module.css";
+import ButtonControls from "../RecurringElements/ButtonControls/ButtonControls";
 
 import whiteboard from "../../assets/img/whiteboard.svg";
 import plant from "../../assets/img/plant.svg";
 import smallTable from "../../assets/img/smallTable.svg";
 import washiOrange from "../../assets/img/washiTapeOrange.svg";
 import washiBordeaux from "../../assets/img/washiTapeBordeaux.svg";
+import pencil from "../../assets/img/pencilBtn.svg";
 import bunting from "../../assets/img/bannerMain.svg";
+import buntingSmall from "../../assets/img/bannerSmall.png";
 import line from "../../assets/img/line.svg";
+import bgNoteThin from "../../assets/img/bgNoteThin.svg";
+import mushrooms from "../../assets/img/mushrooms.svg";
+import butterfly from "../../assets/img/butterfly.svg";
 
 import peer from "../../assets/img/logos/peer.svg";
 import madorTil from "../../assets/img/logos/till.svg";
+// import nextBtn from "../../assets/img/nextBtn.svg";
 
-const EndPage = () => {
+const OpeningPage = () => {
     const navigate = useNavigate();
-
-    const handleRestart = () => {
-        sessionStorage.clear();
-        navigate("/");
-    };
-
-    const handleBackToLearn = () => {
-        navigate("/home");
-    };
+    const [showExplanation, setShowExplanation] = useState(false);
 
     return (
         <div className={styles.page} dir="rtl">
-            <img
-                src={washiOrange}
-                alt=""
-                className={`${styles.washi} ${styles.washiLeft}`}
-            />
+            <img src={washiOrange} alt="" className={`${styles.washi} ${styles.washiLeft}`} />
+            <img src={washiBordeaux} alt="" className={`${styles.washi} ${styles.washiRight}`} />
 
-            <img
-                src={washiBordeaux}
-                alt=""
-                className={`${styles.washi} ${styles.washiRight}`}
-            />
-
-            <div className={styles.logos}>
-                <img src={peer} className={styles.peer} alt="logo" />
-                <img src={madorTil} className={styles.madorTil} alt="logo" />
+            <div className="logos">
+                <img src={peer} className="peer" alt="logo" />
+                <img src={madorTil} className="madorTil" alt="logo" />
             </div>
+
+            <button className={styles.aboutBtn}>
+                אודות
+            </button>
 
             <div className={styles.boardArea}>
                 <div className={styles.boardWrapper}>
                     <img src={whiteboard} alt="" className={styles.whiteboard} />
 
                     <div className={styles.boardContent}>
-                        <p className={styles.endText}>סיימתם את לומדת</p>
 
-                        <div className={styles.buntingWrapper}>
-                            <img src={bunting} alt="" className={styles.buntingImg} />
-                        </div>
+                        {!showExplanation && (
+                            <>
+                                <p className={styles.welcomeText}>ברוכים הבאים ללומדת</p>
 
-                        <div className={styles.buttonsArea}>
-                            <div className={styles.buttonWrapper}>
-                                <img src={line} alt="" className={styles.line} />
+                                <div className={styles.buntingWrapper}>
+                                    <img src={bunting} alt="" className={styles.buntingImg} />
+                                </div>
 
-                                <button
-                                    type="button"
-                                    className={styles.endBtn}
-                                    onClick={handleRestart}
-                                >
-                                    להתחיל מחדש
-                                    <span>←</span>
-                                </button>
+                                <div className={styles.btnDiv}>
+                                    <img src={line} alt="" className={styles.line} />
+                                    <button
+                                        className={styles.startBtn}
+                                        onClick={() => setShowExplanation(true)}
+                                        aria-label="התחל"
+                                    >
+                                        <img src={pencil} alt="" />
+                                    </button>
+                                    <img src={line} alt="" className={styles.line} />
+                                    <p className={styles.startHint}>לחצו על העיפרון כדי להתחיל</p>
+                                </div>
+                            </>
+                        )}
 
-                                <img src={line} alt="" className={styles.line} />
-                            </div>
+                        {showExplanation && (
+                            <>
+                                <div className={styles.buntingWrapperSmall}>
+                                    <img src={buntingSmall} alt="" className={styles.buntingImg} />
+                                </div>
 
-                            <div className={styles.buttonWrapper}>
-                                <img src={line} alt="" className={styles.line} />
+                                <p className={styles.explanationText}>
+                                    {`לומדה זו מתעסקת בימי מיון ומסבירה על חשיבותם, מהלכם
+                                     והעקרונות המנחים להעברתם בצה”ל. הלומדה מרחיבה על
+                                     ההטיות האפשריות שניתן להיתקל בהן בהערכה במהלך יום המיון. 
+                                    בנוסף, יש התייחסות לראיון התעסוקתי ומהלכו בזמן יום המיון.`}
+                                </p>
 
-                                <button
-                                    type="button"
-                                    className={styles.endBtn}
-                                    onClick={handleBackToLearn}
-                                >
-                                    לחזור ללמוד
-                                    <span>←</span>
-                                </button>
+                                <img src={butterfly} alt="butterfly drawing" className={styles.butterfly} />
+                                <img src={mushrooms} alt="mushrooms drawing" className={styles.mushrooms} />
 
-                                <img src={line} alt="" className={styles.line} />
-                            </div>
-                        </div>
+                                <div className={styles.noteBox}>
+                                    <img src={bgNoteThin} alt="bg note" className={styles.bgNoteThin} />
+                                    <p className={styles.noteText}>
+                                        הלומדה מחולקת לשלושה חלקים, בין כל חלק תוכלו לבצע הפסקה
+                                    </p>
+                                </div>
+                            </>
+                        )}
+
                     </div>
                 </div>
             </div>
+
+            {showExplanation && (
+                <ButtonControls
+                    onNext={() => navigate("/home")}
+                    showNext={true}
+                    showBack={false}
+                />
+            )}
 
             <img src={plant} alt="plant" className={styles.plant} />
             <img src={smallTable} alt="table" className={styles.table} />
@@ -95,4 +110,4 @@ const EndPage = () => {
     );
 };
 
-export default EndPage;
+export default OpeningPage;
